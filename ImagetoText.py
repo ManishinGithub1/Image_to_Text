80bc2f0f74565b7ae2904118b9b05ac9553c7697
Y Manish Kumar <116179123+ManishinGithub1@users.noreply.github.com>
2025-01-24 12:10:47 +0530

    Update ImagetoText.py

--- ImagetoText.py
+++ ImagetoText.py
@@ -2,11 +2,11 @@ import pytesseract
 import os
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe" #Copy the tesseract.exe file path and paste
 
 def convert():
     try:
-        img=Image.open('pic.png')
+        img=Image.open('pic.png') #Your image name
     except FileNotFoundError:
         print("File not found")
         return
